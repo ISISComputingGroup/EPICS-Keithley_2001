042efdbb234013c6de4013ebb5e6bfa1b4e12b77
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-10-12 17:48:26 +0100

    Parse readings from the buffer

--- Keithley2001Sup/src/BufferParsing.cpp
+++ Keithley2001Sup/src/BufferParsing.cpp
@@ -15,8 +15,55 @@
 
 long ParseBufferReadings(aSubRecord *prec) {
 
-	/* Returns the first input value back */
-    prec->vala = prec->a;
+	char(*a)[40] = static_cast<char(*)[40]>(prec->a);
+
+	const int reading_values_count = 2;
+	int i = 0;
+
+	while (ParseReading(a[i]) > 0) {
+
+		double value = ParseReading(a[i]);
+		int channel = ParseChannel(a[i + 1]);
+		std::cout << "Setting value " << value << "to channel " << channel << std::endl;
+
+		switch (channel)
+		{
+		case 1:
+			static_cast<double*>(prec->vala)[0] = value;
+			break;
+		case 2:
+			static_cast<double*>(prec->valb)[0] = value;
+			break;
+		case 3:
+			static_cast<double*>(prec->valc)[0] = value;
+			break;
+		case 4:
+			static_cast<double*>(prec->vald)[0] = value;
+			break;
+		case 5:
+			static_cast<double*>(prec->vale)[0] = value;
+			break;
+		case 6:
+			static_cast<double*>(prec->valf)[0] = value;
+			break;
+		case 7:
+			static_cast<double*>(prec->valg)[0] = value;
+			break;
+		case 8:
+			static_cast<double*>(prec->valh)[0] = value;
+			break;
+		case 9:
+			static_cast<double*>(prec->vali)[0] = value;
+			break;
+		case 10:
+			static_cast<double*>(prec->valj)[0] = value;
+			break;
+		default:
+			std::cout << "\nNo channel found for value " << channel << std::endl;
+			break;
+		}
+		i += reading_values_count;
+	}
 	return 0; /* process output links */
 }
 
