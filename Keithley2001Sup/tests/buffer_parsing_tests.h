9e40271131ea752a5413686b558abe1d53ae3c64
Rory-Potter-Tessella <rory.potter@tessella.com>
2018-10-15 16:40:08 +0100

    Refactored tests to be more readable

--- Keithley2001Sup/tests/buffer_parsing_tests.h
+++ Keithley2001Sup/tests/buffer_parsing_tests.h
@@ -14,15 +14,18 @@ class ParseBufferReadingTestSuite : public CxxTest::TestSuite {
 public:
 	void test_that_GIVEN_a_reading_from_the_keithley_2001_with_read_and_unit_elements_THEN_the_reading_is_parsed_out() 
 	{
+		// Given:
 		double expected_result = 9.2;
 		std::string expected_unit = "VDC";
 		std::stringstream reading_stream;
 		reading_stream << std::scientific << expected_result << expected_unit;
-
 		char buffer_reading[40];
 		reading_stream.get(buffer_reading, 40);
 
+		// When:
 		double result = ParseReading(buffer_reading);
+		
+		// Then:
 		TS_ASSERT_EQUALS(result, expected_result);
 	}
 };
@@ -31,20 +34,24 @@ class ParseBufferChannelTestSuite : public CxxTest::TestSuite {
 public:
 	void test_that_GIVEN_the_channel_part_of_a_reading_from_the_keithley_2001_with_chan_and_unit_elements_THEN_the_tenth_channel_is_found() 
 	{
+		// Given:
 		int expected_result = 10;
 		std::string expected_unit = "INTCHAN";
 		std::stringstream reading_stream;
 		reading_stream << std::setfill('0') << std::setw(2) << expected_result << expected_unit;
-
 		char buffer_reading[40];
 		reading_stream.get(buffer_reading, 40);
-
+		
+		// When:
 		int result = ParseChannel(buffer_reading);
+		
+		// Then:
 		TS_ASSERT_EQUALS(result, expected_result);
 	}
 
 	void test_that_GIVEN_the_channel_part_of_a_reading_from_the_keithley_2001_with_chan_and_unit_elements_THEN_the_first_channel_is_found() 
 	{
+		// Given:
 		int expected_result = 1;
 		std::string expected_unit = "INTCHAN";
 		std::stringstream reading_stream;
@@ -53,7 +60,10 @@ public:
 		char buffer_reading[40];
 		reading_stream.get(buffer_reading, 40);
 
+		// When:
 		int result = ParseChannel(buffer_reading);
+		
+		// Then:
 		TS_ASSERT_EQUALS(result, expected_result);
 	}
 };
@@ -63,6 +73,7 @@ class ParseBufferUnitTestSuite : public CxxTest::TestSuite {
 public:
 	void test_that_GIVEN_a_reading_from_the_keithley_2001_with_read_and_unit_elements_THEN_the_unit_is_parsed() 
 	{
+		// Given:
 		double expected_result = 9.2;
 		std::string expected_unit = "VDC";
 		std::stringstream reading_stream;
@@ -71,7 +82,10 @@ public:
 		char buffer_reading[40];
 		reading_stream.get(buffer_reading, 40);
 
+		// When:
 		const char* result = ParseUnit(buffer_reading);
+		
+		// Then:
 		TS_ASSERT_EQUALS(result, expected_unit);
 	}
 };
--- Keithley2001Sup/tests/sequencer_utils_tests.h
+++ Keithley2001Sup/tests/sequencer_utils_tests.h
@@ -11,23 +11,22 @@
 
 class FindActiveChannelsTestSuite : public CxxTest::TestSuite {
 public:
-    void test_that_GIVEN_two_active_channels_THEN_they_are_both_found() 
+    void test_that_GIVEN_three_active_channels_THEN_they_are_both_found() 
 	{
         // Given:
-		int channels [10] = {1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
-		int number_of_active_channels = 2;
+		int channels [10] = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0};
+		int number_of_active_channels = 3;
 		int activated_channels[10] = { 0,0,0,0,0,0,0,0,0,0};
 
 		// When
 		find_active_channels(channels, activated_channels);
 
 		// Then
-		int expected_channels[2] = { 1, 2 };
-		for (int i = 0; i < number_of_active_channels; i++) {
+		int expected_channels[10] = { 1, 5, 9, 0, 0, 0, 0, 0, 0, 0};
+		for (int i = 0; i < 10; i++) {
 		TS_ASSERT_EQUALS(activated_channels[i], expected_channels[i]);
 		}
     }
-
 
 	void test_that_GIVEN_all_active_channels_THEN_they_are_all_found()	
 	{
@@ -41,15 +40,49 @@ public:
 
 		// Then
 		int expected_channels[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		for (int i = 0; i < number_of_active_channels; i++) {
+		for (int i = 0; i < 10; i++) {
 		TS_ASSERT_EQUALS(activated_channels[i], expected_channels[i]);
+		}
+	}
+
+	void test_that_GIVEN_one_active_channels_THEN_it_is_found()
+	{
+		// Given:
+		int channels[10] = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		int number_of_active_channels = 1;
+		int activated_channels[10] = { 0,0,0,0,0,0,0,0,0,0 };
+
+		// When
+		find_active_channels(channels, activated_channels);
+
+		// Then
+		int expected_channels[10] = { 1,0,0,0,0,0,0,0,0,0 };
+		for (int i = 0; i < 10; i++) {
+			TS_ASSERT_EQUALS(activated_channels[i], expected_channels[i]);
+		}
+	}
+
+	void test_that_GIVEN_no_active_channels_THEN_none_are_found()
+	{
+		// Given:
+		int channels[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		int number_of_active_channels = 0;
+		int activated_channels[10] = { 0,0,0,0,0,0,0,0,0,0 };
+
+		// When
+		find_active_channels(channels, activated_channels);
+
+		// Then
+		int expected_channels[10] = { 0,0,0,0,0,0,0,0,0,0 };
+		for (int i = 0; i < 10; i++) {
+			TS_ASSERT_EQUALS(activated_channels[i], expected_channels[i]);
 		}
 	}
 };
 
 class GenerateScanChannelStringTestSuite : public CxxTest::TestSuite {
 public:
-	void test_that_GIVEN_two_active_channels_THEN_the_expected_string_is_found() 
+	void test_that_GIVEN_two_active_channels_THEN_the_expected_string_is_created() 
 	{
 		// Given:
 		int channels[10] = { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -65,7 +98,7 @@ public:
 		TS_ASSERT_EQUALS(scan_channels_string, expected_string);
 	}
 
-	void test_that_GIVEN_all_channels_active_THEN_the_expected_string_is_found() 
+	void test_that_GIVEN_all_channels_active_THEN_the_expected_string_is_created()
 	{
 		// Given:
 		int channels[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
@@ -81,7 +114,7 @@ public:
 		TS_ASSERT_EQUALS(scan_channels_string, expected_string);
 	}
 
-	void test_that_GIVEN_odd_channels_active_THEN_odd_channels_are_added_to_the_string() 
+	void test_that_GIVEN_odd_channels_active_THEN_a_string_with_only_odd_channels_is_created() 
 	{
 		// Given:
 		int channels[10] = { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
@@ -97,7 +130,7 @@ public:
 		TS_ASSERT_EQUALS(scan_channels_string, expected_string);
 	}
 
-	void test_that_GIVEN_a_loop_THEN_we_get_the_correct_string() 
+	void test_that_GIVEN_a_loop_through_the_channels_add_a_new_one_each_time_THEN_the_correct_string_is_generated_each_time() 
 	{
 		// Given:
 		int channels[10] = { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -125,44 +158,6 @@ public:
 			expected += "," + std::to_string(i + 2);
 		}
 	}
-
-	void test_that_GIVEN_a_loop_which_switches_between_5_and_7_channels_THEN_we_dont_get_a_heap_error() 
-	{
-		// Given:
-		int number_of_active_channels = 5;
-		int channels[10] = { 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
-		std::string expected_5 = "1,2,3,4,5";
-		std::string expected_7 = "1,2,3,4,5,6,7";
-		bool five_channels = true;
-		char scan_channels_string[40];
-		int activated_channels[10] = { 0,0,0,0,0,0,0,0,0,0 };
-
-		for (int i = 0; i < 100; i++) {
-			memset(activated_channels, 0, sizeof(activated_channels));
-			find_active_channels(channels, activated_channels);
-
-			// When:
-			TS_ASSERT_THROWS_NOTHING(GenerateActiveChannelsString(activated_channels, number_of_active_channels, scan_channels_string));
-
-			// Then:
-			if (five_channels) {
-				TS_ASSERT_EQUALS(scan_channels_string, expected_5);
-				// Set to scan on severn channels
-				channels[5] = 1;
-				channels[6] = 1;
-				number_of_active_channels = 7;
-				five_channels = false;
-			}
-			else {
-				TS_ASSERT_EQUALS(scan_channels_string, expected_7);
-				// Set to scan on five channels
-				channels[5] = 0;
-				channels[6] = 0;
-				number_of_active_channels = 5;
-				five_channels = true;
-			}
-		}
-	}
 };
 
 class GetNumberOfActiveChannelsTests : public CxxTest::TestSuite {
@@ -177,19 +172,6 @@ public:
 
 		// Then:
 		int expected_result = 0;
-		TS_ASSERT_EQUALS(result, expected_result);
-	}
-
-	void test_that_GIVEN_an_array_with_a_single_one_THEN_the_number_of_active_channels_is_one() 
-	{
-		// Given:
-		int channels[10] = { 1,0,0,0,0,0,0,0,0,0};
-		
-		// When:
-		int result = GetNumberOfActiveChannels(channels);
-		
-		// Then:
-		int expected_result = 1;
 		TS_ASSERT_EQUALS(result, expected_result);
 	}
 
